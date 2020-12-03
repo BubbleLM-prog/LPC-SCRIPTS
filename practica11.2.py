1e62a1aec324af95c0c7cc5196e4515e67846ca7
BubbleLM-prog <75098250+BubbleLM-prog@users.noreply.github.com>
2020-12-03 04:04:27 -0600

    Script con Interfaz (tkinter) --PRACTICA 11--

---
+++ practica11.2.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+from tkinter import *
+
+app = tk.Tk()
+app.title("Practica 11")
+etiqueta = Label(app, text="Hola a Todos!!!")
+boton = Button(app, text="OK!!")
+
+etiqueta.pack()
+boton.pack()
+app.mainloop()
+
+
